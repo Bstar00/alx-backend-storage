a53d0ce0fc27aa23b47605ec46edf831d55a2296
Bstar00 <samueloriola212@gmail.com>
2023-08-09 17:33:28 +0000

    Task 2 and 3

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,6 @@
+-- Retrieve  number of fans by country
+SELECT origin,
+	SUM(fans) as nb_fans
+FROM metal_bands
+group by origin
+order by nb_fans DESC;
---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+--script that lists all bands with Glam rock as their main style
+
+SELECT band_name,
+	ifnull(split, YEAR(CURDATE())) - formed as lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC;
