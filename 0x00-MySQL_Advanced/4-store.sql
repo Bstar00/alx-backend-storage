3a2a8ec2ecd25861fc347b463f41f3dfe19d1633
Bstar00 <samueloriola212@gmail.com>
2023-08-09 18:03:26 +0000

    Task 4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,22 +1,7 @@
--- Create the trigger
-DELIMITER //
-CREATE TRIGGER decrease_quantity_after_order
-AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    DECLARE order_item_id INT;
-    DECLARE order_quantity INT;
-    
-    -- Get the item ID and quantity from the new order
-    SELECT item_id, quantity
-    INTO order_item_id, order_quantity
-    FROM orders
-    WHERE id = NEW.id;
-    
-    -- Decrease the quantity of the item in the items table
-    UPDATE items
-    SET quantity = quantity - order_quantity
-    WHERE id = order_item_id;
-END;
-//
-DELIMITER ;
+-- Buy buy buy
+-- TIGGER
+CREATE TRIGGER buy_buy_buy 
+BEFORE INSERT ON orders
+FOR EACH ROW UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
